f08b885c17d92b57f899c21a79e558866cdb7bf4
Debbie Vo <debbienvo@yahoo.com>
2019-02-07 11:15:05 -0800

    lab 5

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,12 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
-	// Your code goes here
- }
+exports.addFriend = function(request, response) {    
+
+	var name = req.query.name;
+	var description = req.query.description;
+	var imgURL = "http://lorempixel.com/400/400/people";
+	var newFriend = {'name':name,'description':description,'imageURL':imgURL};
+	data.friends.push(newFriend);
+//	console.log(name, description);
+	res.render('index', data);
+ };
